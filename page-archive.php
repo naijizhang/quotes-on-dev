918f1948cdc69f01eac69bf545c690dcb3923689
Naiji Zhang <naijizhang@gmail.com>
2019-03-01 13:57:31 -0800

    Add archive page styling

--- page-archive.php
+++ page-archive.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="content-area content-area-archive">
     <main id="main" class="site-main" role="main">
 
         <h1>Archives</h1>
