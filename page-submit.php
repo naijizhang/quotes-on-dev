867f51f769757b0935e9f15c572caea206d65a9f
Naiji Zhang <naijizhang@gmail.com>
2019-03-01 15:34:26 -0800

    Add css for submit quote page

--- page-submit.php
+++ page-submit.php
@@ -7,30 +7,37 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="content-area content-area-submit">
     <main id="main" class="site-main" role="main">
         <h1>Submit a Quote</h1>
+        <?php if (is_user_logged_in() && current_user_can('edit_post')): ?>
         <form>
-            <div class="author-of-quote">
+            <div class="eachform author-of-quote">
                 <label for="new-author">Author of Quote</label>
-                <input type="text" name="new-author" id="new-author">
+                <input class="each-field" type="text" name="new-author" id="new-author">
             </div>
 
-            <div class="quote-content">
+            <div class="eachform quote-content">
                 <label for="new-content">Quote</label>
-                <input type="text" name="new-content" id="new-content">
+                <input class="each-field" type="text" name="new-content" id="new-content">
             </div>
-            <div class="quote-source">
+            <div class="eachform quote-source">
                 <label for="new-quote-source">Where did you find this quote? (e.g. book name)</label>
-                <input type="text" name="new-quote-source" id="new-quote-source">
+                <input class="each-field" type="text" name="new-quote-source" id="new-quote-source">
             </div>
-            <div class="quote-source-url">
+            <div class="eachform quote-source-url">
                 <label for="new-quote-source-url">Provide the URL of the quote source, if available.</label>
-                <input type="text" name="new-quote-source-url" id="new-quote-source-url">
+                <input class="each-field" type="text" name="new-quote-source-url" id="new-quote-source-url">
             </div>
             <input id="submit-new" type="button" value="Submit Quote">
-		</form>
-		
+        </form>
+        <?php else: ?>
+        <p>Sorry, you must be logged in to submit a quote!</p>
+
+        <a href="<?php echo esc_url(wp_login_url()) ?>">Click here to login.</a>
+
+
+        <?php endif ?>
 
     </main><!-- #main -->
 </div><!-- #primary -->
