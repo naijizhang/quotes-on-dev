124770809b7a9d634bdc8ce93682308dfa6fff15
Naiji Zhang <naijizhang@gmail.com>
2019-03-03 11:24:50 -0800

    Add single page css

--- single.php
+++ single.php
@@ -7,17 +7,16 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area content-area-single">
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<?php get_template_part( 'template-parts/content'); ?>
 
-			<?php the_post_navigation(); ?>
 
 		<?php endwhile; // End of the loop. ?>
-
+		<button type="button" id="get-new-post">Show Me Another!</button>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
