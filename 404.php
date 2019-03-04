1c1e4f13561635648005b9d6a1eeae614de47d3f
Naiji Zhang <naijizhang@gmail.com>
2019-03-03 18:34:57 -0800

    Add 404 page css

--- 404.php
+++ 404.php
@@ -9,22 +9,37 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area content-area-404">
+    <main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
-				</header><!-- .page-header -->
+        <section class="error-404 not-found">
+            <header class="page-header">
+                <h1 class="page-title">
+                    <?php echo esc_html('Oops!'); ?>
+                </h1>
+            </header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try a search?' ); ?></p>
+            <div class="page-content">
+                <p>
+                    <?php echo esc_html('It looks like nothing was found at this location. Maybe try a search?'); ?>
+                </p>
 
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
+                    <div>
+                        <label class="screen-reader-text" for="s">
+                            <?php _x('Search for:', 'label'); ?></label>
+                        <input type="text" placeholder="SEARCH..." value="<?php echo get_search_query(); ?>" name="s" id="s" class="search-text-box" />
+                        <label for="searchsubmit">
+                            <i class="fas fa-search search-icon"></i>
+                        </label>
+                        <input type="submit" id="searchsubmit" value="<?php echo esc_attr_x('Search', 'submit button'); ?>" class="banner-text-btn" />
+                    </div>
+                </form>
+            </div><!-- .page-content -->
+        </section><!-- .error-404 -->
 
-<?php get_footer(); ?>
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?> 
